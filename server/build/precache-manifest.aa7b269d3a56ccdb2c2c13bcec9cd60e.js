2586f33faf1168bcccf78e2bf14c582aaa497d4f
Adrian Ałaszewski <wns2148810@interia.pl>
2020-09-21 13:18:09 +0200

    Initialized procject with working server and template client

---
+++ server/build/precache-manifest.aa7b269d3a56ccdb2c2c13bcec9cd60e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7623343f7d70e8c16c7304c501ea3ba",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d1f4d1d1f488c03d1ff9",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "f67259f3aed9dc589c72",
+    "url": "/static/js/2.26f37104.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.26f37104.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d1f4d1d1f488c03d1ff9",
+    "url": "/static/js/main.0e1705e2.chunk.js"
+  },
+  {
+    "revision": "30cb8b6426f09885e2c0",
+    "url": "/static/js/runtime-main.a9932483.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
