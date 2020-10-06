22f14db864cdc9e2d9d1c8efe871b7fae58b72fc
Adrian Ałaszewski <wns2148810@interia.pl>
2020-10-06 09:58:30 +0200

    New build for server

---
+++ server/build/precache-manifest.f4b3cff57585761c9a47bae9ee8e1e8d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c20fb50e01d002f6d36676a75adc658",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ccf4f8b3e70912633577",
+    "url": "/static/css/2.63f60614.chunk.css"
+  },
+  {
+    "revision": "9ffa49641e41607f452e",
+    "url": "/static/css/main.73f51d57.chunk.css"
+  },
+  {
+    "revision": "ccf4f8b3e70912633577",
+    "url": "/static/js/2.68d6533b.chunk.js"
+  },
+  {
+    "revision": "8e0aa3c7e18eb789384c1cfe7441fd4a",
+    "url": "/static/js/2.68d6533b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9ffa49641e41607f452e",
+    "url": "/static/js/main.7557a0da.chunk.js"
+  },
+  {
+    "revision": "30cb8b6426f09885e2c0",
+    "url": "/static/js/runtime-main.a9932483.js"
+  }
+]);
